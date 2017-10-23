860183d1b5b1bd8efdb7e9dd216e315757b52401
Steve Quatrani <spq2461@gmail.com>
2017-10-22 21:37:08 -0400

    actioncable working

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
     devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
-    resources :chatrooms
-    resources :messages
+    resources :chatrooms do
+        resources :messages
+    end
 end
