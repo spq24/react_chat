34755794a02eea3eb9413b920d7ac8c01ae8d9f4
Steve Quatrani <spq2461@gmail.com>
2017-10-25 12:04:17 -0400

    finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+    protect_from_forgery with: :exception
+
+    def default_url_options
+        { host: "7a2161e7.ngrok.io"}
+    end
 end
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication
+      sign_in @user
+      redirect_to 'http://7a2161e7.ngrok.io/chatrooms'
     else
       session["devise.github_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
